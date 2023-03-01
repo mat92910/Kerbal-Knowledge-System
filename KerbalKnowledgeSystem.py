21b7502b54c504ff1e6442ec4496804410959a8f
Jnarozanski1 <jnarozanski1@laurentian.ca>
2023-02-28 20:41:19 -0500

    comments ^^^^WWW^^^^

--- KerbalKnowledgeSystem.py
+++ KerbalKnowledgeSystem.py
@@ -1,17 +1,26 @@
 import DeltaVMap
 
-#find the node that can be accesses with the given start node and delta v as either a single value or list of delta v of stages
+# find the nodes that can be accesses with the given start node and delta v as either a single value or list of delta v of stages
 def FindAvailableNodeFromDeltaV(Nodes, Start, DeltaV, RoundTrip, Aerobreaking, PlaneChange):
     deltaVmap = DeltaVMap.GetDeltaVMap()
-   
+    DeltaV = sum(DeltaV)
+
+    # start with adding the start node to the return list, if it makes it here it's added
     Nodes.append(Start)
 
+    # get node links to the start node from the map and loop them
     links = DeltaVMap.GetLinksFromDeltaVMap(deltaVmap, Start)
     for point in links:
+
+        # check if in list already to avoid looping 
         if(point in Nodes):
             continue
-        DeltaVLoss = 0
+
+        # get the value for delta v loss calculations    temp[0]=delta v cost  temp[1]=plane change cost   temp[2]=calculate aero bool
         temp = DeltaVMap.GetValuesFromDeltaVMap(deltaVmap, Start, point)
+        DeltaVLoss = 0
+
+        # simple if checks for delta v loss with diffrent checks
         if(Aerobreaking & temp[2]):
             if(RoundTrip):
                 DeltaVLoss += (temp[0]*0.10 + temp[0])
@@ -25,13 +34,14 @@ def FindAvailableNodeFromDeltaV(Nodes, Start, DeltaV, RoundTrip, Aerobreaking, P
         if(PlaneChange):
             DeltaVLoss += temp[1]
 
+        # if the loss is less than the delta v, sent that amount to it's links
         NewDeltaV = (DeltaV - DeltaVLoss)
         if(NewDeltaV >= 0):
            Nodes = FindAvailableNodeFromDeltaV(Nodes, point, NewDeltaV, RoundTrip, Aerobreaking, PlaneChange)
 
     return Nodes
 
-
+# veery fancy main \\\(>>w<<)}}}}
 AvailableNodes = []
 AvailableNodes = FindAvailableNodeFromDeltaV(AvailableNodes, 300, 5500, 0, 0, 0)
 NameList = DeltaVMap.GetNameList()
