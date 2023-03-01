3ce2d0db51de2d0439002988e673fecedf5e56e8
Jnarozanski1 <jnarozanski1@laurentian.ca>
2023-02-28 20:14:16 -0500

    recersive engine to find nodes with deltav

--- KerbalKnowledgeSystem.py
+++ KerbalKnowledgeSystem.py
@@ -1,3 +1,44 @@
+import DeltaVMap
+
 #find the node that can be accesses with the given start node and delta v as either a single value or list of delta v of stages
-def FindAvailableNodeFromDeltaV(Start, DeltaV, RoundTrip, Aerobreaking):
-    AvailableNodes = [Start]
+def FindAvailableNodeFromDeltaV(Nodes, Start, DeltaV, RoundTrip, Aerobreaking, PlaneChange):
+    deltaVmap = DeltaVMap.GetDeltaVMap()
+   
+    Nodes.append(Start)
+
+    links = DeltaVMap.GetLinksFromDeltaVMap(deltaVmap, Start)
+    for i in links:
+        if(i in Nodes):
+            continue
+        v = 0
+        temp = DeltaVMap.GetValuesFromDeltaVMap(deltaVmap, Start, i)
+        if(Aerobreaking):
+            if(temp[2] & (not RoundTrip)):
+                v = v + temp[0]*0.10
+            elif(RoundTrip & (not temp[2])):
+                v = v + temp[0]*2
+            elif(temp[2] & RoundTrip):
+                v = v + temp[0]*0.10 + temp[0]
+            else:
+                v = v + temp[0]
+        else:
+            if(RoundTrip):
+                v = v + temp[0]*2
+            else:
+                v = v + temp[0]
+        if(PlaneChange):
+            v = v + temp[1]
+
+        v = (DeltaV - v)
+        if(v > 0):
+           Nodes = FindAvailableNodeFromDeltaV(Nodes, i, v, RoundTrip, Aerobreaking, PlaneChange)
+
+    return Nodes
+
+
+AvailableNodes = []
+AvailableNodes = FindAvailableNodeFromDeltaV(AvailableNodes, 300, 5500, 0, 0, 0)
+NameList = DeltaVMap.GetNameList()
+for Nodes in AvailableNodes:
+    print(NameList[Nodes])
+#print(AvailableNodes)
